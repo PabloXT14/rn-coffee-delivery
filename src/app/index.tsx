a33618df05760af7c9bd82c7d60e2a7e3a267db7
Pablo Alan <pabloxt14@gmail.com>
2025-06-02 21:48:43 -0300

    chore: config token colors

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+
+import { colors } from '@/styles/colors'
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.gray[900],
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: colors.gray[100],
+  },
+})
