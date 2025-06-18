31b8938d61712c36e98a7583c00b92c009e6a4e1
Pablo Alan <pabloxt14@gmail.com>
2025-06-18 17:17:45 -0300

    feat: add more coffee examples

--- src/components/screens/catalog/carousel-section/index.tsx
+++ src/components/screens/catalog/carousel-section/index.tsx
@@ -6,11 +6,17 @@ import { HighlightCard } from '@/components/shared/highlight-card'
 
 import { styles } from './styles'
 
+const HIGHLIGHT_COFFEES = [
+  COFFEES[0],
+  COFFEES[Math.round(COFFEES.length / 2 - 1)],
+  COFFEES[COFFEES.length - 1],
+]
+
 export function CarouselSection() {
   return (
     <View style={styles.container}>
       <FlatList
-        data={COFFEES}
+        data={HIGHLIGHT_COFFEES}
         keyExtractor={item => item.id}
         renderItem={({ item, index }) => (
           <HighlightCard
--- src/data/coffees.ts
+++ src/data/coffees.ts
@@ -1,8 +1,10 @@
+import { randomUUID } from 'expo-crypto'
+
 import type { Coffee } from '@/@types/coffee'
 
 export const COFFEES: Coffee[] = [
   {
-    id: '1',
+    id: randomUUID(),
     name: 'Latte',
     description: 'Café expresso com o dobro de leite e espuma cremosa',
     price: 990,
@@ -10,7 +12,49 @@ export const COFFEES: Coffee[] = [
     tags: ['tradicionais'],
   },
   {
-    id: '2',
+    id: randomUUID(),
+    name: 'Expresso Tradicional',
+    description: 'O tradicional café feito com água quente e grãos moídos',
+    price: 890,
+    image: require('@/assets/images/coffees/expresso.png'),
+    tags: ['tradicionais'],
+  },
+  {
+    id: randomUUID(),
+    name: 'Expresso Americano',
+    description:
+      'Expresso diluído em água quente, menos intenso que o tradicional',
+    price: 990,
+    image: require('@/assets/images/coffees/americano.png'),
+    tags: ['tradicionais'],
+  },
+  {
+    id: randomUUID(),
+    name: 'Expresso Cremoso',
+    description: 'Café expresso tradicional com espuma cremosa',
+    price: 990,
+    image: require('@/assets/images/coffees/expresso-cremoso.png'),
+    tags: ['tradicionais'],
+  },
+  {
+    id: randomUUID(),
+    name: 'Expresso Gelado',
+    description: 'Bebida preparada com café expresso e cubos de gelo',
+    price: 890,
+    image: require('@/assets/images/coffees/cafe-gelado.png'),
+    tags: ['tradicionais'],
+  },
+  {
+    id: randomUUID(),
+    name: 'Cappuccino',
+    description:
+      'Bebida feita com doses de canela, café expresso, leite e espuma',
+    price: 990,
+    image: require('@/assets/images/coffees/capuccino.png'),
+    tags: ['doces'],
+  },
+  {
+    id: randomUUID(),
     name: 'Mocaccino',
     description: 'Café expresso com calda de chocolate, leite e espuma',
     price: 990,
@@ -18,9 +62,43 @@ export const COFFEES: Coffee[] = [
     tags: ['doces'],
   },
   {
-    id: '3',
+    id: randomUUID(),
+    name: 'Chocolate Quente',
+    description: 'Bebida feita com chocolate dissolvido no leite quente e café',
+    price: 990,
+    image: require('@/assets/images/coffees/chocolate-quente.png'),
+    tags: ['doces'],
+  },
+  {
+    id: randomUUID(),
+    name: 'Cubano',
+    description:
+      'Drink gelado de café expresso com rum, creme de leite e hortelã',
+    price: 990,
+    image: require('@/assets/images/coffees/cubano.png'),
+    tags: ['especiais'],
+  },
+  {
+    id: randomUUID(),
+    name: 'Havaiano',
+    description: 'Bebida adocicada preparada com café e leite de coco',
+    price: 990,
+    image: require('@/assets/images/coffees/havaiano.png'),
+    tags: ['especiais'],
+  },
+  {
+    id: randomUUID(),
+    name: 'Árabe',
+    description: 'Bebida preparada com grãos de café árabe e especiarias',
+    price: 990,
+    image: require('@/assets/images/coffees/arabe.png'),
+    tags: ['especiais'],
+  },
+  {
+    id: randomUUID(),
     name: 'Irlandês',
-    description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly',
+    description:
+      'Café expresso com uísque irlandês, açúcar e chantilly por cima',
     price: 990,
     image: require('@/assets/images/coffees/irlandes.png'),
     tags: ['especiais'],
